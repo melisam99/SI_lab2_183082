a95ebaa5e6a6edf12a6df9c9ec4c4a5983b6ad8a
Melisa Muslioska <muslioskamelisa@gmail.com>
2021-05-27 22:50:57 +0200

    Gradle JAVA Project

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,14 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class SILab2Test extends TestCase {
+
+    @Test
+    void EveryBranchTest(){
+
+    }
+    @Test
+    void MultipleConditionsTest(){
+
+    }
+}
